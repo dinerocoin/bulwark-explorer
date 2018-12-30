fbb9aae51257225dded3745ed0b1aca4aa596b9f
Kolby Moroz <mrmetech@gmail.com>
2018-12-29 17:45:09 -0700

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@
 import Actions from './core/Actions';
 import Component from './core/Component';
 import { connect } from 'react-redux';
-import { Router, } from 'react-router-dom';
+import { Router} from 'react-router-dom';
 import { isAddress, isBlock, isTX } from '../lib/blockchain';
 import { Link, Route, Switch } from 'react-router-dom';
 import promise from 'bluebird';
